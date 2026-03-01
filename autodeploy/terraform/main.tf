8a9aa8c0d16489c75653ebde324d68365c637e5d
Fabio Masciotra <fabmas@microsoft.com>
2026-03-01 02:05:03 +0100

    feat: upgrade VM image to Windows Server 2025 Datacenter Azure Edition
    SQL Server 2025 Enterprise is fully supported on Windows Server 2025.
    SKU: 2025-datacenter-azure-edition (verified available in westeurope)

--- autodeploy/terraform/main.tf
+++ autodeploy/terraform/main.tf
@@ -103,7 +103,7 @@ resource "azurerm_windows_virtual_machine" "vm" {
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
-    sku       = "2022-datacenter-azure-edition"
+    sku       = "2025-datacenter-azure-edition"
     version   = "latest"
   }
 
